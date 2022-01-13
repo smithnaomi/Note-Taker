75fec97a0571f102cc298e2086dd1c76ebb3516f
smithnaomi <88051105+smithnaomi@users.noreply.github.com>
2022-01-13 18:45:13 -0500

    write note fix

--- db/store.js
+++ db/store.js
@@ -3,14 +3,15 @@ const fs = require("fs");
 const { v4: uuid } = require("uuid");
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
+
 class Store {
   // all functions in here
   read() {
     return readFileAsync("db/db.json", "utf-8");
   }
 
-  write() {
-    return writeFileAsync("db/db.json", "utf-8");
+  write(note) {
+    return writeFileAsync("db/db.json", JSON.stringify(note));
   }
 
   getnotes() {
