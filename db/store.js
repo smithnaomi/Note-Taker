716ec38b5e58b0f2388cc35dd8ace22987135326
smithnaomi <88051105+smithnaomi@users.noreply.github.com>
2022-01-13 18:40:28 -0500

    uuid added

--- db/store.js
+++ db/store.js
@@ -1,6 +1,6 @@
 const util = require("util");
 const fs = require("fs");
-const { uuid: uuidv4 } = require("uuid");
+const { v4: uuid } = require("uuid");
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 class Store {
